445cd95424526f9c0db1a40fe6ba60bddbf5f33c
Takuya Shintani <2180264@ecccomp.com>
2021-10-15 10:31:23 +0900

    パッケージ名をsampleからmajangに変更

--- src/majang/Controller.java
+++ src/majang/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package majang;
 
 public class Controller {
 }
--- src/majang/Main.java
+++ src/majang/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package majang;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,8 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("majang.fxml"));
+        primaryStage.setTitle("本格ネット麻雀 焼鳥");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
