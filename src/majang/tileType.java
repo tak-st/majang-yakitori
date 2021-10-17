f76c34d247a10c34e463e2d18781f334a6d9d517
Takuya Shintani <2180264@ecccomp.com>
2021-10-17 15:26:31 +0900

    牌種類列挙型を追加 牌クラスがそれを使用するように仕様変更

---
+++ src/majang/tileType.java
@@ -0,0 +1,15 @@
+package majang;
+
+/**
+ * 牌の種類を定義しています。
+ * <p>
+ * MANZU : 萬子
+ * PINZU : 筒子
+ * SOHZU : 索子
+ * FONPAI : 風牌
+ * SANGEN : 三元牌
+ * ZIPAI : 字牌
+ */
+public enum tileType {
+    MANZU, PINZU, SOHZU, FONPAI, SANGEN, ZIPAI;
+}
