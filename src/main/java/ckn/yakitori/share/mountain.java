604dcc646c4ecd46ff6d2aaa232c2d90e2200c22
Takuya Shintani <2180264@ecccomp.com>
2021-10-23 13:40:32 +0900

    javadocの追加

--- src/main/java/ckn/yakitori/share/mountain.java
+++ src/main/java/ckn/yakitori/share/mountain.java
@@ -2,6 +2,11 @@ package ckn.yakitori.share;
 
 import ckn.yakitori.share.tile.tile;
 
+/**
+ * 山クラスのインターフェースです。
+ * <p>
+ * 実際に山があってもなくても同じ処理をさせるためのものです。
+ */
 public interface mountain {
     int getRemaingTile();
 
