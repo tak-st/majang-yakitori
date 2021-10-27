3ac9e3b7c3fa8d87e2ed3446e72630e2c751bf07
Nakafuji Mizuto <2180417@ecccomp.com>
2021-10-27 14:16:21 +0900

    捨て牌クラスのテストを作成しました。

---
+++ src/test/java/ckn/yakitori/server/discard/discardTileTest.java
@@ -0,0 +1,28 @@
+package ckn.yakitori.server.discard;
+
+import ckn.yakitori.share.tile.tile;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class discardTileTest {
+
+    @Test
+    void getCalledRiichi() {
+        discardTile tile = new discardTile(new tile("1m"), false, false, true);
+        assertFalse(tile.getCalledRiichi());
+    }
+
+    @Test
+    void isImmediately() {
+        discardTile tile = new discardTile(new tile("1m"), true, false, true);
+        assertFalse(tile.isImmediately());
+    }
+
+    @Test
+    void isStealed() {
+        discardTile tile = new discardTile(new tile("1m"), true, false, true);
+        assertTrue(tile.isStealed());
+    }
+}
