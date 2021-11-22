b239eeaf7a1bc2af9d1bb20be57715c4c8ed0226
Nakafuji Mizuto <2180417@ecccomp.com>
2021-11-22 10:28:58 +0900

    現在誰の順目かを返す関数を作成しました。

--- src/main/java/ckn/yakitori/share/round.java
+++ src/main/java/ckn/yakitori/share/round.java
@@ -4,18 +4,36 @@ import ckn.yakitori.server.deadTile;
 import ckn.yakitori.server.mountainEntity;
 import ckn.yakitori.server.mountainType;
 
+/**
+ * 各ラウンド開始時に行う処理です。
+ *
+ * @author mizu
+ * @version 1.0
+ */
 public class round {
 
+    //何順目か
     int turn = 0;
 
-    // 各インスタンス用
+    /**
+     * mountain:山を生成する用<br>
+     * hand:各プレイヤーに手配を配る用<br>
+     * deadTile:王牌を作成する用<br>
+     */
     mountain mountain_tile;
     hand Hand;
     deadTile Dead_tile;
 
+    /**
+     * 各ラウンド開始時に行う処理です。<br><br>
+     * 三麻か四麻を判定し手配作成。<br>
+     * 作成された手配を各プレイヤーに配布。<br>
+     * 王配作成。
+     *
+     * @param Rule 三麻か四麻かを判定する為に使用します。
+     */
     public void roundStart(rule Rule) {
 
-        //何順目か
         turn += 1;
 
         //三麻か四麻の判定
@@ -25,12 +43,38 @@ public class round {
             mountain_tile = new mountainEntity(mountainType.YONMA);
         }
 
-        //各プレイヤーに手配を配る
+        //各プレイヤーに手配を配布
         //TODO マッチクラス作成時に手牌を配る処理を作成
         Hand = new hand(mountain_tile);
 
         //王牌を作成する。
         Dead_tile = new deadTile(mountain_tile);
     }
-    //現在誰の番か返す関数を作成　modで求めるなど
+
+    /**
+     * 三麻か四麻を判定し、現在誰の順目かを返す関数です。
+     *
+     * @param Rule 三麻か四麻かを判定する為に使用します。
+     * @return 現在が誰の順目かを返します。
+     */
+    public int getPlayer(rule Rule) {
+        int playerTurn = 0;
+        if (Rule.isSanma) {
+            switch (turn % 3) {
+                case 0 -> playerTurn = 3;
+                case 1 -> playerTurn = 1;
+                case 2 -> playerTurn = 2;
+            }
+        } else {
+            playerTurn = switch (turn % 4) {
+                case 0 -> 4;
+                case 1 -> 1;
+                case 2 -> 2;
+                case 3 -> 3;
+                default -> playerTurn;
+            };
+        }
+        System.out.println(playerTurn);
+        return playerTurn;
+    }
 }
--- src/test/java/ckn/yakitori/share/roundTest.java
+++ src/test/java/ckn/yakitori/share/roundTest.java
@@ -14,4 +14,30 @@ class roundTest {
         Rule.setSanma(false);
         Round.roundStart(Rule);
     }
+
+    //三麻用テスト
+    @Test
+    void sanma_getPlayer() {
+        round Round = new round();
+        rule Rule = new rule();
+
+        for (int i = 0; i < 3; i++) {
+            Rule.setSanma(true);
+            Round.roundStart(Rule);
+            Round.getPlayer(Rule);
+        }
+    }
+
+    //四麻用テスト
+    @Test
+    void yonma_getPlayer() {
+        round Round = new round();
+        rule Rule = new rule();
+
+        for (int i = 0; i < 4; i++) {
+            Rule.setSanma(false);
+            Round.roundStart(Rule);
+            Round.getPlayer(Rule);
+        }
+    }
 }
