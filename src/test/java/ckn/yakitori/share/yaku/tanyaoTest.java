4af2acbb41b6eccd91e568bda9a2991d87392c59
Takuya Shintani <2180264@ecccomp.com>
2021-12-15 15:03:22 +0900

    鳴き時のチェックにポンではなくカンを使用するように変更（都合が良いので）手牌が上がれない時、上がれないと表示するように変更

--- src/test/java/ckn/yakitori/share/yaku/tanyaoTest.java
+++ src/test/java/ckn/yakitori/share/yaku/tanyaoTest.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 
 import static ckn.yakitori.share.yaku.yakuInfo.TANYAO;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class tanyaoTest {
 
@@ -31,7 +32,7 @@ class tanyaoTest {
             "false, true, false, 1m , 1112223334m111z4m",
             "false, true, false, 1m , 2223334445559m9m",
             "false, false, false, 1m , 1112222333444m5m",
-            "false, true, true, 1m , 1122333444555m2mr",
+            "false, true, true, 1m , 1112333444555m2mr",
             "false, false, true, 2p, 2223334445556p6p",
             "true, true, true, 2p, 2223334445556p6p",
             "true, true, true, 3s, 3334445556667s7s",
@@ -44,25 +45,21 @@ class tanyaoTest {
         hand content = new hand(Hand);
 
         if (expectedIsopen) {
-            content.doPon(Tile);
+            content.doLightKan(Tile);
             content.sortTile();
-            content.setContents(content.getAll()[0], 11);
-            content.setContents(new tile("e"), 1);
+
         }
 
         ArrayList<mentsu> mentsuList = new ArrayList<>();
         statusGroup sg = new statusGroup();
         mentsuPartition M = new mentsuPartition(content);
-        for (int i = 0; i <= M.getMentsuList().size() - 1; i++) {
-            for (mentsu mt : M.getMentsuList(i)) {
-                mentsuList.add(mt);
-            }
+        if (!M.isCanWin()) {
+            fail("上がれない");
         }
-        sg.setMentsuList(mentsuList);
-        sg.setIsopen(mentsuList);
+        sg.setMentsuList(M.getMentsuList(0));
         sg.setCanKuitan(expectedKuitan);
         tanyao Tanyao = new tanyao(sg);
-        assertEquals(expected, Tanyao.isCheckPass(), "チェック結果がおかしい");
+        assertEquals(expected, Tanyao.isCheckPass(), M.getMentsuList(0).toString());
 
     }
 
